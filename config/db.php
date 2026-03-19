18da0007befd023985e9cf2b17beb7e9fb94f368
fullofhope1 <ustprivateemail@gmail.com>
2026-03-18 23:03:31 -0700

    Switching to AeonFree database

--- config/db.php
+++ config/db.php
@@ -1,20 +1,15 @@
 <?php
 // config/db.php
-$servername = "sql111.infinityfree.com";
-$username = "if0_41103629";
-$password = "HpnYShAsAaK";
-$dbname = "if0_41103629_qat";
+$servername = "sql100.hstn.me";
+$username = "mseet_41427862";
+$password = "zt92DPSWefgb"; // Your FTP/vPanel password
+$dbname = "mseet_41427862_qat_erp";
 
-// Set Timezone for PHP
 date_default_timezone_set('Asia/Aden');
 
 try {
-    // Fixed: Changed $host to $servername to match your variable above
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-    // Set Timezone for MySQL Session
     $pdo->exec("SET time_zone = '+03:00'");
 } catch (PDOException $e) {
     die("Database Connection Failed: " . $e->getMessage());
